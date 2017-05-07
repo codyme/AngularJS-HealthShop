e888038194eb1457d6d87d4e9c6f537759b8cc43
Codyme <codyme.git@gmail.com>
2017-05-07 20:48:48 +1000

    adds images

--- js/app.js
+++ js/app.js
@@ -9,6 +9,12 @@ var pizzas = [
         name: 'Hawaiian',
         price: 10.95,
         ingredients: 'Crispy crust, tomato sauce, ham pieces, and pineapple chunks, mixed peppers, mushrooms, and bacon.',
+        images: [
+            {
+                full: './images/hawaiian/full.png',
+                thumb: './images/hawaiian/thumb.png'
+            }
+        ],
         canOrder: true,
         soldOut: false
     },
@@ -16,6 +22,12 @@ var pizzas = [
         name: 'Supreme',
         price: 12,
         ingredients: 'Crispy crust, pepperoni, ham, beef, pork sausage, Italian sausage, red onions, mushrooms, green peppers, and black olives.',
+        images: [
+            {
+                full: './images/supreme/full.png',
+                thumb: './images/supreme/thumb.png'
+            }
+        ],
         canOrder: true,
         soldOut: false
     }
